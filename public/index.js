e3702c266b7808c99932b532dc9ac656c189b228
Catherine Miguel <catmig1992@gmail.com>
2022-10-12 23:56:27 -0700

    npm install, npm start, API key & ChartJS

--- public/index.js
+++ public/index.js
@@ -1,9 +1,21 @@
 async function main() {
+  const timeChartCanvas = document.querySelector("#time-chart");
+  const highestPriceChartCanvas = document.querySelector(
+    "#highest-price-chart"
+  );
+  const averagePriceChartCanvas = document.querySelector(
+    "#average-price-chart"
+  );
 
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+  let response = await fetch(
+    "https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=5e08995e48614c49b5f5e624d8e85f79",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "apikey 5e08995e48614c49b5f5e624d8e85f79",
+      },
+    }
+  );
 }
 
-main()
+main();
