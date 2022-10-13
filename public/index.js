43cee1877989b771a8d0023f756714603b4c64d9
Catherine Miguel <catmig1992@gmail.com>
2022-10-13 04:31:44 -0700

    section 5 complete

--- public/index.js
+++ public/index.js
@@ -18,9 +18,6 @@ async function main() {
   const highestPriceChartCanvas = document.querySelector(
     "#highest-price-chart"
   );
-  const averagePriceChartCanvas = document.querySelector(
-    "#average-price-chart"
-  );
 
   const response = await fetch(
     `https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`
@@ -28,11 +25,11 @@ async function main() {
 
   const result = await response.json();
 
-  const { GME, MSFT, DIS, BNTX } = result;
+  const { GME, MSFT, DIS, BNTX } = mockData;
 
   const stocks = [GME, MSFT, DIS, BNTX];
 
-  stocks.forEach((stock) => stock.values.reverse());
+  //   stocks.forEach((stock) => stock.values);
 
   // Time Chart
   new Chart(timeChartCanvas.getContext("2d"), {
@@ -45,6 +42,32 @@ async function main() {
         backgroundColor: getColor(stock.meta.symbol),
         borderColor: getColor(stock.meta.symbol),
       })),
+    },
+  });
+
+  function findHighest(values) {
+    let highest = 0;
+    values.forEach((value) => {
+      if (parseFloat(value.high) > highest) {
+        highest = value.high;
+      }
+    });
+    return highest;
+  }
+
+  // High Chart
+  new Chart(highestPriceChartCanvas.getContext("2d"), {
+    type: "bar",
+    data: {
+      labels: stocks.map((stock) => stock.meta.symbol),
+      datasets: [
+        {
+          label: "Highest",
+          backgroundColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          borderColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+          data: stocks.map((stock) => findHighest(stock.values)),
+        },
+      ],
     },
   });
 }
